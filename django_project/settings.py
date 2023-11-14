0e7b21fa68d410caf2247c2469d7fdef6311b6ca
Prabaharan Nagappan <126092038+Prabaharan-nagappan@users.noreply.github.com>
2023-11-14 08:27:30 +0530

    Update settings.py database

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,16 +79,16 @@ DATABASES = {
 }
 
 # For Docker/PostgreSQL usage uncomment this and comment the DATABASES config above
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "postgres",
-#         "HOST": "db",  # set in docker-compose.yml
-#         "PORT": 5432,  # default postgres port
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
+    }
+}
 
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-password-validators
